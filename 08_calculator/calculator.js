c51d82d7dc30dc056a480323d86819fa25b72a1b
Henri <henriaernoudts@gmail.com>
2024-01-19 16:10:00 -0300

    8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return product = array.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (let i = 1; i <= a; i++) {
+    total*=i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -31,3 +35,7 @@ module.exports = {
   power,
   factorial
 };
+
+let testArray = [1, 2, 3];
+
+console.log(factorial(0));
